e1d959c6315fae16177dc8d9c3a81642dad11899
Eberth <itec_19@hotmail.com>
2023-06-23 16:53:27 +0200

    develop

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,9 @@ Route::middleware(['auth:api'])->group(function () {
 
     // Route::middleware(['superadmin'])->group(function () {
 
-        Route::get('/users', [UserController::class, 'index']);
-        Route::get('/users/{user}', [UserController::class, 'show']);
-        Route::delete('/users/{user}', [UserController::class, 'destroy']);
+        // Route::get('/users', [UserController::class, 'index']);
+        // Route::get('/users/{user}', [UserController::class, 'show']);
+        // Route::delete('/users/{user}', [UserController::class, 'destroy']);
     // });   
 
     
